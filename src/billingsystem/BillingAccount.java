70e5c7ae7b5bbaad1a18075464f1383173d86a24
Gonçalo Silva <gmrs99.silva@gmail.com>
2023-11-08 19:39:12 +0000

    implemented determinarTarifarioAplicavel function

--- src/billingsystem/BillingAccount.java
+++ src/billingsystem/BillingAccount.java
@@ -4,9 +4,9 @@ import java.util.Date;
 
 public class BillingAccount {
     private String MSISDN;
-    private int bucketA;
-    private int bucketB;
-    private int bucketC;
+    private int bucket1; //cent
+    private int bucket2; //cent
+    private int bucket3; //cent
     private int counterA;
     private int counterB;
     private int counterC;
@@ -24,23 +24,23 @@ public class BillingAccount {
 	public void setMSISDN(String mSISDN) {
 		MSISDN = mSISDN;
 	}
-	public int getBucketA() {
-		return bucketA;
+	public int getBucket1() {
+		return bucket1;
 	}
-	public void setBucketA(int bucketA) {
-		this.bucketA = bucketA;
+	public void setBucket1(int bucket1) {
+		this.bucket1 = bucket1;
 	}
-	public int getBucketB() {
-		return bucketB;
+	public int getBucket2() {
+		return bucket2;
 	}
-	public void setBucketB(int bucketB) {
-		this.bucketB = bucketB;
+	public void setBucket2(int bucket2) {
+		this.bucket2 = bucket2;
 	}
-	public int getBucketC() {
-		return bucketC;
+	public int getBucket3() {
+		return bucket3;
 	}
-	public void setBucketC(int bucketC) {
-		this.bucketC = bucketC;
+	public void setBucket3(int bucket3) {
+		this.bucket3 = bucket3;
 	}
 	public int getCounterA() {
 		return counterA;
--- src/billingsystem/TarifarioManager.java
+++ src/billingsystem/TarifarioManager.java
@@ -1,5 +1,55 @@
 package billingsystem;
 
 public class TarifarioManager {
+	
+	public String determinarTarifarioAplicavel(ChargingRequest chargingRequest, BillingAccount billingAccount) {
+	    String servico = chargingRequest.getService();
+	    String MSISDN = chargingRequest.getMSISDN();
+	    int counterA = billingAccount.getCounterA();
+	    int bucket1 = billingAccount.getBucket1();
+	    int bucket2 = billingAccount.getBucket2();
+	    int bucket3 = billingAccount.getBucket3();
+	    
+	    
+	    if ("A".equals(servico)) {
+	        if (temSaldoSuficienteNoBucketA(MSISDN, bucket1)) {
+	            return "Alfa1";
+	        } else if (temSaldoSuficienteNoBucketB(MSISDN, bucket2) && counterA <= 100) {
+	            return "Alfa2";
+	        } else if (temSaldoSuficienteNoBucketC(MSISDN, bucket3) && counterA <= 100) {
+	            return "Alfa3";
+	        }
+	    } else if ("B".equals(servico)) {
+	    	
+	    }
+
+	    return "Não Elegível - Critérios não atendidos";
+	}
+	
+	
+	private boolean temSaldoSuficienteNoBucketA(String MSISDN, int bucketA) {
+		//1 euros sao 100 centimos
+	    if (bucketA >= 100) { // Verifica se o saldo no buckteA e suficiente para o tarifrio Alfa1
+	        return true;
+	    }
+	    return false;
+	}
+
+
+	private boolean temSaldoSuficienteNoBucketB(String MSISDN, int bucketB) {
+		//10 euros sao 1000 centimos
+		if(bucketB >= 1000) { // Verifica se o saldo no buckteB e suficiente para o tarifrio Alfa2
+			return true; 
+		}
+		return false;
+	}
+
+	private boolean temSaldoSuficienteNoBucketC(String MSISDN, int bucketC) {
+		//10 euros sao 1000 centimos
+		if(bucketC >= 1000) { // Verifica se o saldo no bucktec e suficiente para o tarifrio Alfa3
+			return true; 
+		}
+		return false;
+	}
 
 }
